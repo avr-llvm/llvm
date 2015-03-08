977aa380e1e50dc49cd8e04c95a33d4725514622
Edwin Amsler <edwinguy@gmail.com>
2015-03-07 21:34:29 -0600

    Fixed test for ADC instruction

--- test/MC/AVR/inst-adc.s
+++ test/MC/AVR/inst-adc.s
@@ -3,12 +3,12 @@
 
 foo:
 
-  adc r12, 3
+  adc r12, r3
   adc r10, r0
   adc r5,  r4
   adc r13, r13
 
 ; CHECK: adc r12, r3                 ; encoding: [0xc3,0x1c]
 ; CHECK: adc r10, r0                 ; encoding: [0xa0,0x1c]
-; CHECK: adc r5, r4                  ; encoding: [0x54,0x1e]
+; CHECK: adc r5, r4                  ; encoding: [0x54,0x1c]
 ; CHECK: adc r13, r13                ; encoding: [0xdd,0x1c]
