317752390fd4184c1f57afd9f9970b40b8fe5ccd
Dylan McKay <dylanmckay34@gmail.com>
2015-03-08 21:04:56 +1300

    [AVR] Fixed the SBR MC test

--- test/MC/AVR/inst-sbr.s
+++ test/MC/AVR/inst-sbr.s
@@ -3,12 +3,12 @@
 
 foo:
 
-  sbr r17, 0b11010000
-  sbr r24, 0xbe
-  sbr r20, 0xad
-  sbr r31, 0x00
+  sbr r17, 208
+  sbr r24, 190
+  sbr r20, 173
+  sbr r31, 0
   
-; CHECK: sbr r17, 0b11010000           ; encoding: [0x10,0x6d]
-; CHECK: sbr r24, 0xbe                 ; encoding: [0x8e,0x6b]
-; CHECK: sbr r20, 0xad                 ; encoding: [0x4d,0x6a]
-; CHECK: sbr r31, 0x00                 ; encoding: [0xf0,0x60]
+; CHECK: sbr r17, 208                  ; encoding: [0x10,0x6d]
+; CHECK: sbr r24, 190                  ; encoding: [0x8e,0x6b]
+; CHECK: sbr r20, 173                  ; encoding: [0x4d,0x6a]
+; CHECK: sbr r31, 0                    ; encoding: [0xf0,0x60]
