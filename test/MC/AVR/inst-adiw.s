b99a573cff1d3854e9ad212031e5fa03fac7cd6e
Dylan McKay <dylanmckay34@gmail.com>
2015-03-08 21:41:54 +1300

    [AVR] Fixed ADIW/SBIW MC tests
    Disassembly always prints pointer registers as their lower 8-bit GPR

--- test/MC/AVR/inst-adiw.s
+++ test/MC/AVR/inst-adiw.s
@@ -12,11 +12,11 @@ foo:
   adiw Z,  63
   adiw Z,  3
 
-; CHECK: adiw X,  12                 ; encoding: [0x1c,0x96]
-; CHECK: adiw X,  63                 ; encoding: [0xdf,0x96]
+; CHECK: adiw r26,  12                 ; encoding: [0x1c,0x96]
+; CHECK: adiw r26,  63                 ; encoding: [0xdf,0x96]
 
-; CHECK: adiw Y,  17                 ; encoding: [0x61,0x96]
-; CHECK: adiw Y,  0                  ; encoding: [0x20,0x96]
+; CHECK: adiw r28,  17                 ; encoding: [0x61,0x96]
+; CHECK: adiw r28,  0                  ; encoding: [0x20,0x96]
 
-; CHECK: adiw Z,  63                 ; encoding: [0xff,0x96]
-; CHECK: adiw Z,  3                  ; encoding: [0x33,0x96]
+; CHECK: adiw r30,  63                 ; encoding: [0xff,0x96]
+; CHECK: adiw r30,  3                  ; encoding: [0x33,0x96]
--- test/MC/AVR/inst-sbiw.s
+++ test/MC/AVR/inst-sbiw.s
@@ -12,11 +12,11 @@ foo:
   sbiw Z,  63
   sbiw Z,  47
 
-; CHECK: sbiw X,  54                 ; encoding: [0xd6,0x97]
-; CHECK: sbiw X,  63                 ; encoding: [0xdf,0x97]
+; CHECK: sbiw r26,  54                 ; encoding: [0xd6,0x97]
+; CHECK: sbiw r26,  63                 ; encoding: [0xdf,0x97]
 
-; CHECK: sbiw Y,  52                 ; encoding: [0xe4,0x97]
-; CHECK: sbiw Y,  0                  ; encoding: [0x20,0x97]
+; CHECK: sbiw r28,  52                 ; encoding: [0xe4,0x97]
+; CHECK: sbiw r28,  0                  ; encoding: [0x20,0x97]
 
-; CHECK: sbiw Z,  63                 ; encoding: [0xff,0x97]
-; CHECK: sbiw Z,  47                 ; encoding: [0xbf,0x97]
+; CHECK: sbiw r30,  63                 ; encoding: [0xff,0x97]
+; CHECK: sbiw r30,  47                 ; encoding: [0xbf,0x97]
