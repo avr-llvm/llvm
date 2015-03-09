d17aeaf18beb860e9c8e1adcf9584a0b4006d832
Dylan McKay <dylanmckay34@gmail.com>
2015-03-10 01:01:34 +1300

    [AVR] Fixed typo in BRBS/BRBC MC tests

--- test/MC/AVR/inst-brbs.s
+++ test/MC/AVR/inst-brbs.s
@@ -6,6 +6,6 @@ foo:
   brbs 3, 8
   brbs 0, 0
 
-; CHECK: brbs 3, 7                   ; encoding: [0x1b,0xf0]
+; CHECK: brbs 3, 8                   ; encoding: [0x1b,0xf0]
 ; CHECK: brbs 0, 0                   ; encoding: [0xf0,0xf3]
 
