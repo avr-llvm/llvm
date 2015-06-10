fe7c851d78f8a7e291e78244efd3d46911cac800
Dylan McKay <dylanmckay34@gmail.com>
2015-06-10 17:09:23 +1200

    [AVR] Added labels to `rjmp` MC test

--- test/MC/AVR/inst-rjmp.s
+++ test/MC/AVR/inst-rjmp.s
@@ -5,10 +5,20 @@ foo:
 
   rjmp  .+2
   rjmp  .-2
+  rjmp  foo
   rjmp  .+8
+  rjmp  end
   rjmp  .+0
+end:
+  rjmp .-4
+  rjmp .-6
 
+; CHECK: foo:
 ; CHECK: rjmp  .+2                   ; encoding: [0x01,0xc0]
 ; CHECK: rjmp  .-2                   ; encoding: [0xff,0xcf]
+; CHECK: rjmp  foo                   ; encoding: [A,0b1100AAAA]
 ; CHECK: rjmp  .+8                   ; encoding: [0x04,0xc0]
+; CHECK: rjmp  end                   ; encoding: [A,0b1100AAAA]
 ; CHECK: rjmp  .+0                   ; encoding: [0x00,0xc0]
+; CHECJ: rjmp  .-4                   ; encoding: [0xfe,0xcf]
+; CHECK: rjmp  .-6                   ; encoding: [0xfd,0xcf]
