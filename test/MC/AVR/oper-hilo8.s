9c7c5212863ed8b06323ad07fa84bad29ff051f8
Dylan McKay <dylanmckay34@gmail.com>
2015-06-13 07:13:42 +1200

    [AVR] Improved `oper-hilo8` MC test
    We were neglecting to check the encodings of the instructions with
    label operands.

--- test/MC/AVR/oper-hilo8.s
+++ test/MC/AVR/oper-hilo8.s
@@ -8,12 +8,19 @@ foo:
     ldi r24, lo8(0x23)
     ldi r24, hi8(0x2342)
 
-    ldi r24, lo8(foo)
-    ldi r24, hi8(foo)
+bar:
+
+    ldi r24, lo8(bar)
+    ldi r24, hi8(bar)
 
 ; CHECK: ldi	r24, lo8(66)            ; encoding: [0x82,0xe4]
 ; CHECK: ldi	r24, lo8(9026)          ; encoding: [0x82,0xe4]
 
 ; CHECK: ldi	r24, lo8(35)            ; encoding: [0x83,0xe2]
 ; CHECK: ldi	r24, hi8(9026)          ; encoding: [0x83,0xe2]
+
+; CHECK: ldi	r24, lo8(bar)           ; encoding: [0x80'A',0xe0]
+                                        ; fixup A - offset: 0, value: lo8(bar), kind: fixup_lo8_ldi
+; CHECK: ldi	r24, hi8(bar)           ; encoding: [0x80'A',0xe0]
+                                        ; fixup A - offset: 0, value: hi(bar), kind: fixup_hi8_ldi
 
