795cf59153c276a0f838f49889a2df235ebe7526
Dylan McKay <dylanmckay34@gmail.com>
2015-06-16 20:03:27 +1200

    [AVR] Fixed tests

--- test/MC/AVR/inst-adiw.s
+++ test/MC/AVR/inst-adiw.s
@@ -3,20 +3,20 @@
 
 foo:
 
-  adiw X,  12
-  adiw X,  63
+  adiw r26,  12
+  adiw r26,  63
   
-  adiw Y,  17
-  adiw Y,  0
+  adiw r28,  17
+  adiw r28,  0
   
-  adiw Z,  63
-  adiw Z,  3
+  adiw r30,  63
+  adiw r30,  3
 
-; CHECK: adiw X,  12                 ; encoding: [0x1c,0x96]
-; CHECK: adiw X,  63                 ; encoding: [0xdf,0x96]
+; CHECK: adiw r26,  12               ; encoding: [0x1c,0x96]
+; CHECK: adiw r26,   63               ; encoding: [0xdf,0x96]
 
-; CHECK: adiw Y,  17                 ; encoding: [0x61,0x96]
-; CHECK: adiw Y,  0                  ; encoding: [0x20,0x96]
+; CHECK: adiw r28,  17               ; encoding: [0x61,0x96]
+; CHECK: adiw r28,  0                ; encoding: [0x20,0x96]
 
-; CHECK: adiw Z,  63                 ; encoding: [0xff,0x96]
-; CHECK: adiw Z,  3                  ; encoding: [0x33,0x96]
+; CHECK: adiw r30,  63               ; encoding: [0xff,0x96]
+; CHECK: adiw r30,  3                ; encoding: [0x33,0x96]
--- test/MC/AVR/inst-sbiw.s
+++ test/MC/AVR/inst-sbiw.s
@@ -3,29 +3,29 @@
 
 foo:
 
-  sbiw X,  54
-  sbiw X,  63
+  sbiw r26, 54
+  sbiw X,   63
   
-  sbiw Y,  52
-  sbiw Y,  0
+  sbiw 28,  52
+  sbiw r28, 0
   
-  sbiw Z,  63
-  sbiw Z,  47
+  sbiw r30, 63
+  sbiw Z,   47
 
   sbiw r24,     1
   sbiw r25:r24, 2
   sbiw r27:r26, 3
 
 
-; CHECK: sbiw X,  54                 ; encoding: [0xd6,0x97]
-; CHECK: sbiw X,  63                 ; encoding: [0xdf,0x97]
+; CHECK: sbiw r26,  54                 ; encoding: [0xd6,0x97]
+; CHECK: sbiw r26,  63                 ; encoding: [0xdf,0x97]
 
-; CHECK: sbiw Y,  52                 ; encoding: [0xe4,0x97]
-; CHECK: sbiw Y,  0                  ; encoding: [0x20,0x97]
+; CHECK: sbiw r28,  52                 ; encoding: [0xe4,0x97]
+; CHECK: sbiw r28,  0                  ; encoding: [0x20,0x97]
 
-; CHECK: sbiw Z,  63                 ; encoding: [0xff,0x97]
-; CHECK: sbiw Z,  47                 ; encoding: [0xbf,0x97]
+; CHECK: sbiw r30,  63                 ; encoding: [0xff,0x97]
+; CHECK: sbiw r30,  47                 ; encoding: [0xbf,0x97]
 
-; CHECK: sbiw r24,  1                ; encoding: [0x01,0x97]
-; CHECK: sbiw r24,  2                ; encoding: [0x02,0x97]
-; CHECK: sbiw X,    3                ; encoding: [0x13,0x97]
+; CHECK: sbiw r24,  1                  ; encoding: [0x01,0x97]
+; CHECK: sbiw r24,  2                  ; encoding: [0x02,0x97]
+; CHECK: sbiw r26,  3                  ; encoding: [0x13,0x97]
