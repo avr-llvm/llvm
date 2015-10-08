30b98e837b816571200690114cc8473b10d2b6a8
Duncan P. N. Exon Smith <dexonsmith@apple.com>
2015-10-08 23:49:46 +0000

    IR: Remove implicit iterator conversions from lib/IR, NFC
    Stop converting implicitly between iterators and pointers/references in
    lib/IR.  For convenience, I've added a `getIterator()` accessor to
    `ilist_node` so that callers don't need to know how to spell the
    iterator class (i.e., they can use `X.getIterator()` instead of
    `Function::iterator(X)`).
    
    I'll eventually disallow these implicit conversions entirely, but
    there's a lot of code, so it doesn't make sense to do it all in one
    patch.  One library or so at a time.
    
    Why?  To root out cases of `getNextNode()` and `getPrevNode()` being
    used in iterator logic.  The design of `ilist` makes that invalid when
    the current node could be at the back of the list, but it happens to
    "work" right now because of a bug where those functions never return
    `nullptr` if you're using a half-node sentinel.  Before I can fix the
    function, I have to remove uses of it that rely on it misbehaving.
    (Maybe the function should just be deleted anyway?  But I don't want
    deleting it -- potentially a huge project -- to block fixing
    ilist/iplist.)
    
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@249782 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/IR/AsmWriter.cpp
+++ lib/IR/AsmWriter.cpp
@@ -819,7 +819,7 @@ void SlotTracker::processFunction() {
   for(Function::const_arg_iterator AI = TheFunction->arg_begin(),
       AE = TheFunction->arg_end(); AI != AE; ++AI)
     if (!AI->hasName())
-      CreateFunctionSlot(AI);
+      CreateFunctionSlot(&*AI);
 
   ST_DEBUG("Inserting Instructions:\n");
 
@@ -2637,7 +2637,7 @@ void AssemblyWriter::printFunction(const Function *F) {
     Out << " {";
     // Output all of the function's basic blocks.
     for (Function::const_iterator I = F->begin(), E = F->end(); I != E; ++I)
-      printBasicBlock(I);
+      printBasicBlock(&*I);
 
     // Output the function's use-lists.
     printUseLists(F);
