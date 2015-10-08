20f3cdf67e1fa3198da2716e09af572c1aeaa760
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2015-10-08 21:21:24 +0000

    Add Triple::isAndroid().
    This is a simple refactoring that replaces Triple.getEnvironment()
    checks for Android with Triple.isAndroid().
    
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@249750 91177308-0d34-0410-b5e6-96231b3b80d8

--- include/llvm/ADT/Triple.h
+++ include/llvm/ADT/Triple.h
@@ -512,6 +512,9 @@ public:
            getOS() == Triple::PS4;
   }
 
+  /// \brief Tests whether the target is Android
+  bool isAndroid() const { return getEnvironment() == Triple::Android; }
+
   /// @}
   /// @name Mutators
   /// @{
